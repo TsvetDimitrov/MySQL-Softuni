fb294d787bee3438ea27a1217a1a8267758d29a7
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2024-09-11 00:35:58 +0300

    tableRelations-LAB: Provide all solutions for the LAB.

--- Table Relations/Table Relations - LAB/2.TripOrganization.sql
+++ Table Relations/Table Relations - LAB/2.TripOrganization.sql
@@ -1,1 +1,5 @@
 -- 2. Trip Organization --
+
+SELECT v.driver_id, v.vehicle_type, CONCAT(c.first_name, ' ', c.last_name) AS `driver_name`
+FROM campers AS c
+JOIN vehicles AS v ON v.driver_id = c.id
--- Table Relations/Table Relations - LAB/4.DeleteMountains.sql
+++ Table Relations/Table Relations - LAB/4.DeleteMountains.sql
@@ -1,1 +1,36 @@
 -- 4. Delete Mountains --
+
+CREATE TABLE `mountains` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45)
+);
+
+
+CREATE TABLE `peaks` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45),
+`mountain_id` INT,
+CONSTRAINT `fk_peaks_mountains`
+FOREIGN KEY(`mountain_id`)
+REFERENCES `mountains`(`id`)
+ON DELETE CASCADE
+);
+
+-- OR --
+
+CREATE TABLE `mountains` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45)
+);
+
+CREATE TABLE `peaks` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(45),
+`mountain_id` INT
+);
+
+ALTER TABLE `peaks` 
+ADD CONSTRAINT 
+FOREIGN KEY(`mountain_id`)
+REFERENCES TABLE `mountains`(`id`)
+ON DELETE CASCADE;
