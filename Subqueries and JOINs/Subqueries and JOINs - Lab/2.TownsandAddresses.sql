1bd6f87ed444fd9c59a5ff7d13a6fee360417bde
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2024-09-13 01:16:52 +0300

    Subqueries and JOINs - Lab: Create solutions for Subqueries and JOIN's - LAB

--- Subqueries and JOINs/Subqueries and JOINs - Lab/2.TownsandAddresses.sql
+++ Subqueries and JOINs/Subqueries and JOINs - Lab/2.TownsandAddresses.sql
@@ -1,1 +1,6 @@
 -- 2. Towns and Addresses --
+
+SELECT towns.town_id, towns.name AS 'town_name', addresses.address_text FROM towns
+JOIN addresses ON towns.town_id = addresses.town_id
+WHERE towns.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY towns.town_id, addresses.address_id;
