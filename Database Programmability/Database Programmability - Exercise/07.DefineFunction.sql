57305fb5ff7365cd86f74d21c9626e3897452d07
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2024-09-17 23:37:39 +0300

    Database Programmability-Exercise: Add last exercise solutions.

--- Database Programmability/Database Programmability - Exercise/07.DefineFunction.sql
+++ Database Programmability/Database Programmability - Exercise/07.DefineFunction.sql
@@ -1,1 +1,12 @@
 -- 07. Define Function --
+-- Please submit the solution solution without the Delimeter and the symbols --
+
+DELIMITER ==
+CREATE FUNCTION ufn_is_word_comprised(`set_of_letters` varchar(50), `word` varchar(50))
+    RETURNS TINYINT
+    DETERMINISTIC
+BEGIN
+    RETURN (
+        SELECT word REGEXP CONCAT('^[', set_of_letters, ']+$')
+    );
+END==
