ff8989aba7f6efb6bfad014c40cc2b66348e567b
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2024-09-23 21:29:44 +0300

    Exam prep: Create solutions for the first exam preparation.

--- Exam Preparations/Exam Preparation 1/04.Extractallmilitaryjourneys.sql
+++ Exam Preparations/Exam Preparation 1/04.Extractallmilitaryjourneys.sql
@@ -1,1 +1,5 @@
 -- 04. Extract all military journeys --
+
+SELECT id, journey_start, journey_end FROM journeys
+WHERE purpose = 'Military'
+ORDER BY journey_start;
--- Exam Preparations/Exam Preparation 1/09.Extractthelesspopularjob.sql
+++ Exam Preparations/Exam Preparation 1/09.Extractthelesspopularjob.sql
@@ -1,1 +1,13 @@
 -- 09. Extract the less popular job --
+
+SELECT tc.job_during_journey AS `job_name`
+FROM travel_cards AS tc
+WHERE tc.journey_id = (
+    SELECT j.id
+    FROM journeys AS j
+    ORDER BY DATEDIFF(journey_end, journey_start) DESC
+    LIMIT 1
+    )
+GROUP BY tc.job_during_journey
+ORDER BY COUNT(tc.`job_during_journey`)
+LIMIT 1;
