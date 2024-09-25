0a20ae23f0c846adbae6d8591ac1bad24ed79fee
Tsvetomir Dimitrov <dimitrovv32@gmail.com>
2024-09-25 23:31:51 +0300

    examPrep: Add all solutions for the exam.

--- Exam Preparations/Exam Preparation 1/11.Modifyspaceship.sql
+++ Exam Preparations/Exam Preparation 1/11.Modifyspaceship.sql
@@ -1,1 +1,23 @@
 -- 11. Modify spaceship --
+
+CREATE PROCEDURE udp_modify_spaceship_light_speed_rate(spaceship_name VARCHAR(50), light_speed_rate_increse INT)
+BEGIN
+    DECLARE spaceship_exists INT DEFAULT 0;
+
+    START TRANSACTION;
+    SELECT COUNT(*) INTO spaceship_exists
+    FROM spaceships
+    WHERE name = spaceship_name;
+
+    IF spaceship_exists > 0 THEN
+        UPDATE spaceships
+        SET light_speed_rate = light_speed_rate + `light_speed_rate_increse`
+        WHERE name = `spaceship_name`;
+
+        COMMIT;
+    ELSE
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Spaceship you are trying to modify does not exist.';
+    END IF;
+END
